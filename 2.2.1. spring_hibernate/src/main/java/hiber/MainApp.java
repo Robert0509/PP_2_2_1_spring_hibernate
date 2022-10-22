f022f91607760188dbb5f19419c3bc111b66f48b
Robert0509 <robert.boroyan@yandex.ru>
2022-10-22 22:28:11 +0300

    1.1

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -14,11 +15,15 @@ public class MainApp {
             new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
+      Car car1 = new Car("Mini", 111);
+      Car car2 = new Car("Audi", 444);
+      Car car3 = new Car("BMW", 999);
+      Car car4 = new Car("Hudson", 000);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru",car1));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru",car2));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru",car3));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru",car4));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -28,6 +33,7 @@ public class MainApp {
          System.out.println("Email = "+user.getEmail());
          System.out.println();
       }
+      System.out.println(userService.getUserByCar("Mini",111));
 
       context.close();
    }
---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,67 @@
+package hiber.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "car")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long id;
+    @Column
+    public String model;
+
+    @Column
+    public int series;
+    @OneToOne
+    private User user;
+
+    public Car() {
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+}
