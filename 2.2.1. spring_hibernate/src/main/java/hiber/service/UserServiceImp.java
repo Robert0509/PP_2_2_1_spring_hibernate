ade76a934b8490f8823ad91bca2511c552bb41c1
Robert0509 <robert.boroyan@yandex.ru>
2022-10-24 14:39:05 +0300

    1.3

--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserServiceImp.java
@@ -26,8 +26,8 @@ public class UserServiceImp implements UserService {
 
    @Transactional(readOnly = true)
    @Override
-   public List<User> listUsers() {
-      return userDao.listUsers();
+   public List<User> getUsers() {
+      return userDao.getUsers();
    }
 
    @Transactional(readOnly = true)
