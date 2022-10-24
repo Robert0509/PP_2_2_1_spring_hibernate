118e52b6f1c6de8746b6999c05be76975f3ef740
Robert0509 <robert.boroyan@yandex.ru>
2022-10-24 14:29:10 +0300

    1.2

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -1,6 +1,7 @@
 package hiber.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "car")
@@ -54,6 +55,19 @@ public class Car {
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return id == car.id && series == car.series && Objects.equals(model, car.model) && Objects.equals(user, car.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, model, series, user);
     }
 
     @Override
